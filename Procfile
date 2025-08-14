7877f4dd9033c4de2a444eab7168d46662ea5b58
felegy <felegy@hvg.hu>
2025-08-14 07:55:36 +0000

    + ADD standard heroku php project

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: heroku-php-nginx -i ${PHP_INI-config/php.ini} -C ${NGINX_CONF-config/nginx.conf} ${WEBROOT-web}
---
+++ web/app.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "<h1 align=\"center\" >Welcome kpack CNB PHP test image</h1>";
+
+require_once dirname(__FILE__).'/app/info.php';
